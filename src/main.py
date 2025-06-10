3711125b3c879419e7a215ec222146c36601d87b
Sourav Mohanty <sourav.mohanty93@gmail.com>
2025-06-10 14:25:21 +0530

    Testing jenkins trigger

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return "Hello Sourav 5!"
+    return "Hello Sourav 6!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
